32b64897d9da1a4b5ba80bafcb0df731355bdf70
Pedro Augusto <pedro.aug2808@gmail.com>
2026-04-30 18:08:14 -0300

    ci: Uses php 8.4.1
    fix: Restores 'home' route on redirect due to existing tests
    
    fix(tests): ExampleTest.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', 'dashboard');
+Route::redirect('/', 'dashboard')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
 
-require __DIR__ . '/settings.php';
+require __DIR__.'/settings.php';
